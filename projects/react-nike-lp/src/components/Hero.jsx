dbe6fe24859938e8323f2d8ac37d4e22066a8b48
João Vitor Andrade de Melo <jmelo@bionexo.com>
2025-09-09 17:57:50 -0300

    Primeiro commit, portifolio

---
+++ projects/react-nike-lp/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+const HeroSection = () => {
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR
+                SHOES. YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH
+                OUR SHOES.
+            </p>
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+            <div className="shopping">
+                 <p>Also available on</p>
+                 <div className="brand-icons">
+                    <img src="public/images/flipkart.png" alt="flipkart-logo"/>
+                    <img src="public/images/amazon.png" alt="amazon-logo"/>
+                </div>
+            </div>
+            <div className="hero-image"></div>
+            </div>
+            <div className="hero-image">
+                <img src="public/images/shoe_image.png" alt="shoe-image" />
+            </div>
+        </main>
+    )
+}
+
+
+
+export default HeroSection;
